52b753e60777ad604fabec4d861c8fc538702ffd
Eugene Zabolotny <eugene.zabolotny@gmail.com>
2018-08-08 19:41:02 +0300

    data emitting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very-secret-key'
@@ -19,7 +19,8 @@ def test_disconnect():
 @socketio.on('message')
 def handle_message(msg):
     print('Message: ' + msg)
-    send(msg, broadcast=True)
+    # send(msg, broadcast=True)
+    emit('play', 'http://192.168.52.10:81/api/src/movies/1/SampleVideo_1280x720_5mb.mp4', broadcast=True)
 
 
 if __name__ == '__main__':
